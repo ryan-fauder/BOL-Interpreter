8304086e967ca46761111aae5cd33add170b5853
Luiz Fernando <60102191+luizfernandofo@users.noreply.github.com>
2023-01-22 12:29:43 -0300

    finalização de set_method e inicio de set_class

--- src/describer-test.lua
+++ src/describer-test.lua
@@ -1,8 +1,11 @@
 require "describer"
+require "utils"
 
-local test_method_table = {
+local test_class_table = {
+  "class Pessoa",
+    "vars num",
     "method calc(x)",
-    "vars y",
+    "vars y, z",
     "begin",
       "y = x + self.num",
       "io.print(y)",
@@ -11,9 +14,38 @@ local test_method_table = {
     "end-method"
 }
 
+local test_method_table = {
+    "method calc(x)",
+    "vars y, z",
+    "begin",
+      "y = x + self.num",
+      "io.print(y)",
+      "y = new Base",
+      "return y",
+    "end-method"
+}
+
+local function test_set_class()
+  local describer = Get_describer()
+
+  local described_class_table = describer:set_class(test_class_table)
+
+  Print_table(described_class_table.methods)
+
+end
+
 local function test_set_method()
   local describer = Get_describer()
 
-  describer:set_method()
+  local described_method_table = describer:set_method(test_method_table)
+  
+  print(described_method_table.name)
+  Print_table(described_method_table.params)
+  Print_table(described_method_table.vars)
+  Print_table(described_method_table.body)
   
 end
+
+
+test_set_class()
+--test_set_method()
--- src/describer.lua
+++ src/describer.lua
@@ -1,4 +1,7 @@
 require "patterns"
+require "args"
+
+require "utils"
 
 local Describer = {
   classes = {},
@@ -12,22 +15,25 @@ end
 
 
 function Describer:set_class(class_block)
-  local class_table = {
+  local described_class_table = {
     attr = {},
-    methods = {
-        ["method_name"] = {},
-        ["method_name1"] = {},
-        ["method_name2"] = {},
-    },
+    methods = {},
     _prototype = nil
   }
-  local index = 1
-  local match = { class_block[index]:match("[%s]*method[%s]+([%a]+)%(.-%)[%s]*") }
 
-  if #match > 1 then
-    local method_block, index = Read_method_block(class_block, index)
-    self.set_method(method_block)
+  local method_block
+  local index = 1
+  local match = class_block[index]:match("[%s]*method[%s]+([%a]+)%(.-%)[%s]*")
+
+  while match do
+    method_block, index = Read_method_block(class_block, index)
+
+    table.insert(described_class_table.methods, match, self.set_method(method_block))
+    
+    match = class_block[index]:match("[%s]*method[%s]+([%a]+)%(.-%)[%s]*")
   end
+
+  return described_class_table
 end
 
 
@@ -37,7 +43,8 @@ end
 
 
 function Describer:set_method(method_block)
-  local method_table = {
+
+  local described_method_table = {
     name = nil,
     params = nil,
     vars = nil,
@@ -45,15 +52,39 @@ function Describer:set_method(method_block)
   }
 
   local i = 1
-  local name, params = method_block[i]:match("[%s]*method[%s]+([%a]+)%((.-)%)[%s]*")
-  i = i + 1
-  
-  method_table.name = name
-  method_table.params = Arg_var_list(params)
-  return method_table
+
+  local name, params = method_block[i]:match("^[%s]*method[%s]+([%a]+)%((.-)%)[%s]*$") -- verificar se é necessário mudar essa regex em "patterns", incluído '^' e '$'
+
+  described_method_table.name = name
+
+  described_method_table.params = Arg_var_list(params)
+
+  i = i + 1 -- avança para a linha de vars ou begin, caso não tenha vars
+
+  local vars_keyword, vars = method_block[i]:match("^[%s]*([%l]+)[%s]+(.-)[%s]*$") -- verificar se é necessário mudar essa regex em "patterns", incluído '^' e '$'
+
+  if vars_keyword ~= nil and vars_keyword < "vars" or vars_keyword > "vars" then
+    Error("Erro em Describer:set_method: Esperado 'vars', lido " .. "'" .. vars_keyword .. "'")
+  else
+    described_method_table.vars = Arg_var_list(vars)
+
+    i = i + 1 -- avança para a linha do begin
+  end
+
+  if not method_block[i]:match(_Method_body_begin_pattern_) then
+    Error("Erro em Describer:set_method: Esperado um 'begin' na funcao " .. "'" .. name .. "'")
+  end
+
+  i = i + 1 -- avança para a primeira linha do method-body
+
+  local method_body = { table.unpack(method_block, i, #method_block - 1) }
+
+  described_method_table.body = method_body
+
+  return described_method_table
 end
 
 
 function Describer:get_method()
-    
+  
 end
